2976f4b46eb3e2ec94de5750f130c3dd6d035f94
Mohit Bharti <mohitbharti373@gmail.com>
2023-10-25 03:59:28 +0530

    made test routes

---
+++ controllers/testController.js
@@ -0,0 +1,8 @@
+
+export const testPostController = (req,res)=>{
+  const {name}=req.body;
+  res.status(200).send("your name is mohit");
+};
+
+
+// export default {testPostController}
--- index.js
+++ index.js
@@ -2,13 +2,23 @@
 // const path=require("path");
 import  express  from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+
+
 import connectDB from './config/db.js';
+import testRouter from "./routes/testRouter.js"
 
 dotenv.config();
 
 connectDB();
 
 const app =express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/v1/test", testRouter);
+
 const PORT = process.env.PORT || 8000;
 
 // app.set("view engine","ejs");
