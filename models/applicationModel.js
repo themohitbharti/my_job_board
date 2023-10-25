46e22a21544f899e5b960ebd93ca09cdfb0084b8
Mohit Bharti <mohitbharti373@gmail.com>
2023-10-26 04:16:38 +0530

    created application schema

---
+++ models/applicationModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema({
+  job_seeker_id: {
+    type: String,
+    required: [true, "job seeker id is required"],
+  },
+  job_id: {
+    type: String,
+      required: [true, "Job id is required"],
+    
+  },
+ 
+  application_date: {
+    type: String,
+    required: [true, " application deadline is required"],
+  },
+  status: {
+    type: String,
+    enum: ["pending", "accepted","rejected"],
+    default: "pending",
+  },
+
+
+},{timestamps:true});
+
+export default mongoose.model("Application", applicationSchema);
